4792a347bc09016a26bd5315dd5e31b637f7cc1f
Yu Wu <ywuu0152@student.monash.edu>
2020-10-27 06:13:39 +1100

    (D.3): Email + (E.2): Bulk Email

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using FIT5032_Week08A.Models;
+using FIT5032_Week08A.Utils;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,5 +29,54 @@ namespace FinalAssignment.Controllers
 
             return View();
         }
+        public ActionResult Send_Email()
+        {
+            return View(new SendEmailViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Send_Email(SendEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    String toEmail = model.ToEmail;
+                    String subject = model.Subject;
+                    String contents = model.Contents;
+
+                    var attachment = Request.Files["attachment"];
+
+                    if (attachment.ContentLength > 0)
+                    {
+                        String filePath = Path.Combine(Server.MapPath("~/Content/Attachment/"), attachment.FileName);
+                        attachment.SaveAs(filePath);
+
+                        EmailSender es = new EmailSender();
+                        //es.Send(toEmail, subject, contents);
+                        es.SendWithAttachment(toEmail, subject, contents, filePath, attachment.FileName);
+                    }
+                    else
+                    {
+                        EmailSender es = new EmailSender();
+                        es.Send(toEmail, subject, contents);
+                    }
+
+                    ViewBag.Result = "Email has been send.";
+
+                    ModelState.Clear();
+
+                    return View(new SendEmailViewModel());
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+
     }
+
 }
---
+++ Utils/EmailSender.cs
@@ -0,0 +1,45 @@
+﻿using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace FIT5032_Week08A.Utils
+{
+    public class EmailSender
+    {
+
+        //Please use your API KEY here.
+        private const String API_KEY = "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";
+
+        public void Send(String toEmail, String subject, String contents)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("wuyu240411@gmail.com", "Yu Wu");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            var response = client.SendEmailAsync(msg);
+        }
+
+        public void SendWithAttachment(String toEmail, String subject, String contents, String filePath, String fileName)
+        {
+            var client = new SendGridClient(API_KEY);
+            var from = new EmailAddress("wuyu240411@gmail.com", "Yu Wu");
+            var to = new EmailAddress(toEmail, "");
+            var plainTextContent = contents;
+            var htmlContent = "<p>" + contents + "</p>";
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+
+            var attachment = File.ReadAllBytes(filePath);
+            msg.AddAttachment(fileName, Convert.ToBase64String(attachment));
+
+            var response = client.SendEmailAsync(msg);
+        }
+
+    }
+}
